1772318fd668485616869b76781d69db2f431aa7
chlar <choimk0512@gmail.com>
2024-01-25 21:32:47 +0900

    add log

--- omok_complete.c
+++ omok_complete.c
@@ -6,30 +6,42 @@ void prt_board(int* board, int n) ;  // 오목판 출력 함수
 
 int main()
 {
-    int i, j, k, x, y, n, turn=0, turnNum=0 ;
+    int i, j, k ;  // for문 변수 
+		int n, x, y ; // 오목판 크기, x좌표(열), y좌표(행) 
+		int turn=0, turnNum=0, result ;  // 흑/백 차례, 수, 결과 
 
 		/* 오목판 크기 설정 */ 
-    reput_board: printf("  choose board size (9x9 ~ 15x15) : ") ;
+    setting_board: printf("  오목판 크기를 정하세요(9x9 ~ 15x15) : ") ;
     scanf("%d", &n) ;
     if(n<9)
     {
-        printf("  The board size must be at least 9x9\n") ;
-        goto reput_board ;  // 오목판 크기 재입력 
+        printf("  오목판의 크기는 최소 9x9이상이어야 합니다.\n\n") ;
+        goto setting_board ;  // 오목판 크기 재입력 
     }
-    int board[n][n] ;
-    for(i=0;i<n;i++) for(j=0;j<n;j++) board[i][j] = 0 ;  // 오목판 배열 0으로 초기화 
+    else if(n>15)
+    {
+    	printf("  오목판의 크기는 최대 15x15이하여야 합니다.\n\n") ;
+    	goto setting_board ;
+		}
+    int board[n][n] ;  // 오목 판 배열 선언
+		int log[n][2] ;  // 경기 진행을 기록하는 로그 배열 선언 
+    for(i=0;i<n;i++) for(j=0;j<n;j++) board[i][j] = 0 ;  // 오목판 배열 0으로 초기화
+    for(i=0;i<n;i++) { log[i][0] = 0 ; log[i][1] = 0 ; }  // 로그 배열 0으로 초기화 
 
-    prt_board(board, n) ;  // 오목판 출력. [][]를 입력하지 않음으로서 board배열의 주소를 매개변수로 넣음 
+    prt_board(board, n) ;  // 오목판 출력. [][]를 입력하지 않음으로서 board배열의 주소를 매개변수로 넣음
+    printf("  << 조작법 : 돌을 놓을 때, [ x좌표(열) y좌표(행) ] 형태로 입력. >>\n\n") ;
 
     /* play game */
     while(turnNum<n*n)  // 진행 횟수가 오목판 크기보다 작거나 같을 때 실행 
     {
         if(turn==0)  // 검은돌 순서
         {
-            turnNum++ ;  // 진행 횟수 +1 
             reput_b: printf("  black >> ") ;
-            scanf("%x %x", &x, &y) ;  // 좌표 입력. 편의 위해 [열] [행]으로 입력 
-            x-- ; y-- ; // 1부터 시작 -> 0부터 시작. 사용자 편의 
+            scanf("%x %x", &x, &y) ;  // 좌표 입력. 편의 위해 [열] [행]으로 입력
+            log[turnNum][0] = x ;  // x좌표(열) 기록 
+						log[turnNum][1] = y ;  // y좌표(행) 기록 
+            x-- ; y-- ; // 1부터 시작 -> 0부터 시작. 사용자 편의
+						turnNum++ ;  // 진행 횟수 +1 
             
             if(x<0 || x>n || y<0 || y>n)  // 잘못된 입력 
             {
@@ -47,7 +59,8 @@ int main()
                 prt_board(board, n) ;  // 결과 출력
                 if(check(board, n, x, y)==1)
                 {
-                    printf("\n  [ BLACK WIN!! ]") ;
+                    printf("  [ BLACK WIN!! ]") ;
+                    result = 0 ; 
                     break ;
                 }
                 turn = 1 ;  // 순서 전환 
@@ -55,10 +68,13 @@ int main()
         }
         else  // 흰돌 순서 
         {
-            turnNum++ ;
+
             reput_w: printf("  white >> ") ;
             scanf("%x %x", &x, &y) ;
+            log[turnNum][0] = x ; 
+						log[turnNum][1] = y ;
             x-- ; y-- ;
+            turnNum++ ;
             
             if(x<0 || x>n || y<0 || y>n)
             {
@@ -77,13 +93,36 @@ int main()
                 if(check(board, n, x, y)==1)
                 {
                     printf("  [ WHITE WIN!! ]") ;
+                    result = 1 ;
                     break ;
                 }
                 turn = 0 ;
             }
         }
     }
-    if(turnNum==n*n) printf("  [ TIE ]\n") ;  // 무승부 
+    if(turnNum==n*n) { printf("  [ TIE ]\n\n") ; result = 2 ; }  // 무승부
+    
+    
+    /* 게임 기록 저장 */ 
+    FILE* fp = fopen("omok_log.txt","a");  // txt 파일 생성 
+    
+    fprintf(fp, "------------------------------\n") ;
+    fprintf(fp, "오목판 크기 : %dx%d\n\n", n, n) ;
+    for(i=0;i<turnNum;i++)
+    {
+    	if(i%2==0) fprintf(fp, "흑 - ") ;
+    	else fprintf(fp, "백 - ") ;
+    	fprintf(fp, "%d  %d\n", log[i][0], log[i][1]) ;  // 좌표 출력 
+		}
+		switch(result)  // 게임 결과 출력 
+		{
+			case 0: fprintf(fp, "\nBLACK WIN\n") ; break ;
+			case 1: fprintf(fp, "\nWHITE WIN\n") ; break ;
+			case 2: fprintf(fp, "\nTIE\n") ; break ;
+		}
+		fprintf(fp, "------------------------------\n\n\n") ;
+    fclose(fp);  // 파일 작업 마침 
+    printf("\n  대국 로그가 저장되었습니다.");
 
     return 0 ;
 }
